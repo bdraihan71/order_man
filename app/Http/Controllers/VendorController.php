fb7b91d1ed1dc01042792d8c15c6f6916a163d1e
Your Name <raihan@techynaf.com>
2019-05-06 16:39:23 +0600

    vendor crud done

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,82 +3,141 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Vendor;
 
 class VendorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $vendors = Vendor::all();
+        return view('vendor.index', compact('vendors'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('vendor.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'company_name' => 'required',
+            'office_address' => 'required',
+            'trade_license_number' => 'required',
+            'owner_name' => 'required',
+            'owner_nid_number' => 'required',
+            'owner_current_house_address' => 'required',
+            'owner_contact_number_primary' => 'required',
+            'owner_contact_number_secondary' => 'required',
+            'primary_contact_person_position' => 'required',
+            'primary_contact_person_number_primary' => 'required',
+            'primary_contact_person_number_secondary' => 'required',
+            'secondary_contact_person_position' => 'required',
+            'secondary_contact_person_number_primary' => 'required',
+            'secondary_contact_person_number_secondary' => 'required',
+            'documents_google_drive_link' => 'required',
+            'overall_review' => 'required',
+            'review_by_service' => 'required',
+            'added_by' => 'required',
+            'add_note' => 'required',
+            'reviewed_by' => 'required',
+            'review_note' => 'required',
+            'margin' => 'required',
+        ]);
+
+        Vendor::create([
+            'company_name' => $request->company_name,
+            'office_address' => $request->office_address,
+            'trade_license_number' => $request->trade_license_number,
+            'owner_name' => $request->owner_name,
+            'owner_nid_number' => $request->owner_nid_number,
+            'owner_current_house_address' => $request->owner_current_house_address,
+            'owner_contact_number_primary' => $request->owner_contact_number_primary,
+            'owner_contact_number_secondary' => $request->owner_contact_number_secondary,
+            'primary_contact_person_position' => $request->primary_contact_person_position,
+            'primary_contact_person_number_primary' => $request->primary_contact_person_number_primary,
+            'primary_contact_person_number_secondary' => $request->primary_contact_person_number_secondary,
+            'secondary_contact_person_position' => $request->secondary_contact_person_position,
+            'secondary_contact_person_number_primary' => $request->secondary_contact_person_number_primary,
+            'secondary_contact_person_number_secondary' => $request->secondary_contact_person_number_secondary,
+            'documents_google_drive_link' => $request->documents_google_drive_link,
+            'overall_review' => $request->overall_review,
+            'review_by_service' => $request->review_by_service,
+            'added_by' => $request->added_by,
+            'add_note' => $request->add_note,
+            'reviewed_by' => $request->reviewed_by,
+            'review_note' => $request->review_note,
+            'margin' => $request->margin,
+        ]);
+
+        return redirect(route('vendors.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $vendor = Vendor::find($id);
+        return view('vendor.edit', compact('vendor'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'company_name' => 'required',
+            'office_address' => 'required',
+            'trade_license_number' => 'required',
+            'owner_name' => 'required',
+            'owner_nid_number' => 'required',
+            'owner_current_house_address' => 'required',
+            'owner_contact_number_primary' => 'required',
+            'owner_contact_number_secondary' => 'required',
+            'primary_contact_person_position' => 'required',
+            'primary_contact_person_number_primary' => 'required',
+            'primary_contact_person_number_secondary' => 'required',
+            'secondary_contact_person_position' => 'required',
+            'secondary_contact_person_number_primary' => 'required',
+            'secondary_contact_person_number_secondary' => 'required',
+            'documents_google_drive_link' => 'required',
+            'overall_review' => 'required',
+            'review_by_service' => 'required',
+            'added_by' => 'required',
+            'add_note' => 'required',
+            'reviewed_by' => 'required',
+            'review_note' => 'required',
+            'margin' => 'required',
+        ]);
+        
+        $vendor = Vendor::find($id);
+        $vendor->company_name = $request->get('company_name');
+        $vendor->office_address = $request->get('office_address');
+        $vendor->trade_license_number = $request->get('trade_license_number');
+        $vendor->owner_name = $request->get('owner_name');
+        $vendor->owner_nid_number = $request->get('owner_nid_number');
+        $vendor->owner_current_house_address = $request->get('owner_current_house_address');
+        $vendor->owner_contact_number_primary = $request->get('owner_contact_number_primary');
+        $vendor->owner_contact_number_secondary = $request->get('owner_contact_number_secondary');
+        $vendor->primary_contact_person_position = $request->get('primary_contact_person_position');
+        $vendor->primary_contact_person_number_primary = $request->get('primary_contact_person_number_primary');
+        $vendor->primary_contact_person_number_secondary = $request->get('primary_contact_person_number_secondary');
+        $vendor->secondary_contact_person_position = $request->get('secondary_contact_person_position');
+        $vendor->secondary_contact_person_number_primary = $request->get('secondary_contact_person_number_primary');
+        $vendor->secondary_contact_person_number_secondary = $request->get('secondary_contact_person_number_secondary');
+        $vendor->documents_google_drive_link = $request->get('documents_google_drive_link');
+        $vendor->overall_review = $request->get('overall_review');
+        $vendor->review_by_service = $request->get('review_by_service');
+        $vendor->added_by = $request->get('added_by');
+        $vendor->add_note = $request->get('add_note');
+        $vendor->reviewed_by = $request->get('reviewed_by');
+        $vendor->review_note = $request->get('review_note');
+        $vendor->margin = $request->get('margin');
+        $vendor->save();
+
+        return redirect(route('vendors.index'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $vendor = Vendor::find($id);
+        $vendor->delete();
+        return redirect(route('vendors.index'));
     }
 }
---
+++ resources/views/vendor/edit.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Vendor</div>
+        <div class="card-body">
+          <form method="post" action="{{route('vendors.update', $vendor->id)}}">
+            @csrf
+            @method('patch')
+
+            <label>Company Name</label>
+            <input class="form-control" type="text" name="company_name" value="{{$vendor->company_name}}"></input>
+            <br>
+
+            <label>Office Address</label>
+            <input class="form-control" type="text" name="office_address" value="{{$vendor->office_address}}"></input>
+            <br>
+
+            <label>Trade License Number</label>
+            <input class="form-control" type="text" name="trade_license_number" value="{{$vendor->trade_license_number}}"></input>
+            <br>
+
+            <label>Owner Name</label>
+            <input class="form-control" type="text" name="owner_name" value="{{$vendor->owner_name}}"></input>
+            <br>
+
+            <label>Nid Number</label>
+            <input class="form-control" type="text" name="owner_nid_number" value="{{$vendor->owner_nid_number}}"></input>
+            <br>
+
+            <label>Current Address</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" name="owner_current_house_address" rows="3">{{$vendor->owner_current_house_address}}</textarea>
+            <br>
+
+            <label>Contact Number Primary</label>
+            <input class="form-control" type="text" name="owner_contact_number_primary" value="{{$vendor->owner_contact_number_primary}}"></input>
+            <br>
+
+            <label>Contact Number Secondary</label>
+            <input class="form-control" type="text" name="owner_contact_number_secondary" value="{{$vendor->owner_contact_number_secondary}}"></input>
+            <br>
+
+            <label>primary Contact person Position</label>
+            <input class="form-control" type="text" name="primary_contact_person_position" value="{{$vendor->primary_contact_person_position}}"></input>
+            <br>
+
+            <label>Primary Contact Person Number Primary</label>
+            <input class="form-control" type="text" name="primary_contact_person_number_primary" value="{{$vendor->primary_contact_person_number_primary}}"></input>
+            <br>
+
+            <label>Primary Contact Person Number Secondary</label>
+            <input class="form-control" type="text" name="primary_contact_person_number_secondary" value="{{$vendor->primary_contact_person_number_secondary}}"></input>
+            <br>
+
+            <label>Secondary Contact Person Position</label>
+            <input class="form-control" type="text" name="secondary_contact_person_position" value="{{$vendor->secondary_contact_person_position}}"></input>
+            <br>
+
+            <label>Secondary Contact Person Number Primary</label>
+            <input class="form-control" type="text" name="secondary_contact_person_number_primary" value="{{$vendor->secondary_contact_person_number_primary}}"></input>
+            <br>
+
+            <label>Secondary Contact Person Number Secondary</label>
+            <input class="form-control" type="text" name="secondary_contact_person_number_secondary" value="{{$vendor->secondary_contact_person_number_secondary}}"></input>
+            <br>
+
+            <label>Documents Google Drive Link</label>
+            <input class="form-control" type="text" name="documents_google_drive_link" value="{{$vendor->documents_google_drive_link}}"></input>
+            <br>
+
+            <label>Overall Review</label>
+            <input class="form-control" type="number" name="overall_review" value="{{$vendor->overall_review}}"></input>
+            <br>
+
+            <label>Review By Service</label>
+            <input class="form-control" type="number" name="review_by_service" value="{{$vendor->review_by_service}}"></input>
+            <br>
+
+            <label>Added By</label>
+            <input class="form-control" type="number" name="added_by" value="{{$vendor->added_by}}"></input>
+            <br>
+
+            <label>Add Note</label>
+            <input class="form-control" type="text" name="add_note" value="{{$vendor->add_note}}"></input>
+            <br>
+
+            <label>Reviewed By</label>
+            <input class="form-control" type="number" name="reviewed_by" value="{{$vendor->reviewed_by}}"></input>
+            <br>
+
+            <label>Review Note</label>
+            <input class="form-control" type="text" name="review_note" value="{{$vendor->review_note}}"></input>
+            <br>
+
+            <label>Margin</label>
+            <input class="form-control" type="number" name="margin" value="{{$vendor->margin}}"></input>
+            <br>
+
+
+            <button class="btn btn-primary" type="submit">Update Vendor</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
