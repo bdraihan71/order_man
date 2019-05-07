adfdad321af9107a64c31fe39f9baa43d40b4400
Your Name <raihan@techynaf.com>
2019-05-07 12:50:40 +0600

    applied  softdelete

--- app/Vendor.php
+++ app/Vendor.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vendor extends Model
 {
     protected $guarded = [];
-    
+
+    use SoftDeletes;
+
     public function services(){
         return $this->hasMany('App\Service');
     }
--- database/migrations/2019_04_25_053152_create_vendors_table.php
+++ database/migrations/2019_04_25_053152_create_vendors_table.php
@@ -15,6 +15,7 @@ class CreateVendorsTable extends Migration
     {
         Schema::create('vendors', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->softDeletes();
             $table->text('company_name');
             $table->text('office_address');
             $table->text('trade_license_number')->nullable();
