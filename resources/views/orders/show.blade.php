8253cec1b126ac70383620d610c978dd6d1f2135
Tasnim Mortoza <tasnimmortoza@Tasnims-MacBook-Air.local>
2019-05-10 21:45:36 +0600

    Design fixed

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -2,17 +2,16 @@
 
 @section('content')
     <div class="row mb-4">
-        <div class="col-md-11">
-            <h1>Order no: {{ $order->id }}</h1> <br>
+        <div class="col-md-6">
+            <h1>Order no: {{ $order->id }}<span class="border border-warning h4 ml-3 ">{{ $order->action == null ? "Booked" : ($order->action < 0 ? "Cancelled" : "Provided") }}</span></h1>
             <h3>
-                Customer: {{ $order->customer->name }} <span class="float-right">Booked at: {{ $order->booked_at }}</span>
+                Customer: {{ $order->customer->name }} <span class="float-right text-primary">Booked at: {{ $order->booked_at }}</span>
             </h3>
         </div>
-        <div class="col-md-1">
-            {{ $order->action == null ? "Booked" : ($order->action < 0 ? "Cancelled" : "Provided") }}
+        <div class="col-md-6">
             <a onclick="return confirm('Are you sure you want to delete this item?');" href="{{ route('delete-order', ['order' => $order->id]) }}" class="float-right ml-3"><i class="fa fas fa-trash"></i></a>
             <a href="{{ route('orders.edit', ['order' => $order->id]) }}" class="float-right"><i class='far fa-edit'></i></a>
-            <a href="{{ route('order.action', ['order' => $order->id]) }}" class="btn btn-primary float-right">Take Action</a>
+            <a href="{{ route('order.action', ['order' => $order->id]) }}" class="float-right mr-4"><i class="fa fa-tools"></i></a>
         </div>
     </div>
 
@@ -38,37 +37,37 @@
 
     @foreach ($order->items as $item)
         <div class="row mb-4">
-            <div class="col-md-3">
-                {{ $item->service->title }}
+            <div class="col-md-5">
+                <p><Span class="small">Service -</span> {{ $item->service->title }}</p>
             </div>
             <div class="col-md-2">
-                {{ $item->service_price }}
+                <p><Span class="small">Price -</span> {{ $item->service_price }}</p>
             </div>
             <div class="col-md-2">
-                {{ $item->service_commission }}
+                <p><Span class="small">Commission -</span> {{ $item->service_commission }}</p>
             </div>
             <div class="col-md-1">
-                {{ $item->review }}
-                <i class= "fas fa-star"></i>
+                <p><Span class="small">Review -</span> {{ $item->review }} <i class= "fas fa-star"></i></p>
             </div>
-            <div class="col-md-1">
-                <a onclick="return confirm('Are you sure you want to delete this item?');" href="{{ route('delete-item', ['item' => $item->id]) }}" class="float-right ml-3"><i class='fa fas fa-trash'></i></a>
-                <a href="{{ route('edit-order-item', ['item' => $item->id]) }}" class="float-right"><i class='far fa-edit'></i></a>
+            <div class="col-md-2">
+                <Span class="small">Action -</span>
+                <a href="{{ route('edit-order-item', ['item' => $item->id]) }}"><i class='far fa-edit'></i></a>
+                <a onclick="return confirm('Are you sure you want to delete this item?');" href="{{ route('delete-item', ['item' => $item->id]) }}" class="ml-3"><i class='fa fas fa-trash'></i></a>
             </div>
         </div>
 
         <div class="row mb-4">
-            <div class="col-md-2">
-                Delivery Time: {{ $item->delivery_time }}
+            <div class="col-md-3">
+                <small>Delivery Time: </small> {{ $item->delivery_time }}
             </div>
             <div class="col-md-2">
-                Type: {{ $item->type }}
+                <small>Type:</small> {{ $item->type }}
             </div>
-            <div class="col-md-8">
-                Comment By Category Manager: {{ $item->comment_by_category_manager }}
+            <div class="col-md-5">
+                <small>Comment By Category Manager:</small> {{ $item->comment_by_category_manager }}
             </div>
-            <div class="col-md-8">
-                Vendor: {{ $item->vendor->company_name }}
+            <div class="col-md-2">
+                <small>Vendor:</small> {{ $item->vendor->company_name }}
             </div>
         </div>
 
