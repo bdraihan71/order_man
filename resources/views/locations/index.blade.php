35249ec5f76a535b3d743ca2c44b05f9e2515e8b
Asif Arefin Bonny <bonny@techynaf.com>
2019-06-01 15:34:55 +0600

    Location section has been mobile optimized

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -4,12 +4,12 @@
 <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Locations
-                        <a href="{{route('locations.create')}}" class="btn btn-primary">Create Location</a>
+                <div class="card bg-transparent my-3">
+                    <div class="card-header lead">Locations
+                        <a href="{{route('locations.create')}}" class="btn btn-outline-info float-right">Create Location</a>
                     </div>
 
-                    <div class="card-body">
+                    <div class="card-body table-responsive">
                         <table class="table table-bordered">
                             <tr>
                                 <td>Name</td>
@@ -19,12 +19,12 @@
                             <tr>
                                 <td>{{ $location->name }}</td>
                                 <td>
-                                    <a class="btn btn-primary" href="{{route('locations.show', ['location' => $location->id])}}">View</a>
-                                    <a class="btn btn-info" href="{{route('locations.edit', ['location' => $location->id])}}">Edit</a>
+                                    <a class="btn btn-primary my-1" href="{{route('locations.show', ['location' => $location->id])}}">View</a>
+                                    <a class="btn btn-info my-1" href="{{route('locations.edit', ['location' => $location->id])}}">Edit</a>
                                     <form action="{{ route('locations.destroy', $location->id)}}" onclick="return confirm('Are you sure?')" method="post" style="display: inline;">
                                     @csrf
                                     @method('delete')
-                                    <button class="btn btn-danger" type="submit">delete</button>
+                                    <button class="btn btn-danger my-1" type="submit">delete</button>
                                 </form>
                                 </td>
                             </tr>
