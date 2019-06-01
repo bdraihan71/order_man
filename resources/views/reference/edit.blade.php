7d9527b2a0b03840237ff8cfd3f600498f45c155
Asif Arefin Bonny <bonny@techynaf.com>
2019-06-01 15:40:08 +0600

    Reference section has been mobile optimized

--- resources/views/reference/edit.blade.php
+++ resources/views/reference/edit.blade.php
@@ -4,16 +4,16 @@
 <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card bg-transparent my-3">
                     <div class="card-header">Users</div>
                     <div class="card-body">
                     <form method="post" action="{{route('reference.update', $reference->id)}}">
                             @csrf
                             @method('patch')
                             <label>Name</label>
-                            <input class="form-control" type="text" name="name" value="{{$reference->name}}"></input>
+                            <input class="form-control bg-transparent" type="text" name="name" value="{{$reference->name}}"></input>
                             <br>
-                            <button class="btn btn-primary" type="submit">Update Reference</button>
+                            <button class="btn btn-info" type="submit">Update Reference</button>
                         </form>
                     </div>
                 </div>
