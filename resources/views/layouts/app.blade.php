e7cd30aa2cbdd6f87499bf8493084f01e1cce676
Asif Arefin Bonny <bonny@techynaf.com>
2019-10-06 18:43:26 +0600

    Add plus icon in the nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,9 @@
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ml-auto">
                 <li class="nav-item my-auto">
+                    <a class="nav-link" href="{{ route('orders.create') }}"><i class="fas fa-plus-circle fa-2x"></i></a>
+                </li>
+                <li class="nav-item my-auto">
                     <a class="nav-link" href="/customers">Customer</a>
                 </li>
                 <li class="nav-item my-auto">
