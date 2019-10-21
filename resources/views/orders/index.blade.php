f8cd8e5cdb34a5dff744d4b7777aa686fc5adfb0
Gulshan Jubaed Prince <prince@techynaf.com>
2019-10-21 17:44:55 +0600

    Add quantity and total to order view

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -28,6 +28,8 @@
                 <tr>
                     <th scope="col">Service</th>
                     <th scope="col">Price</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Total</th>
                     <th scope="col">Commission</th>
                     <th scope="col">Customer Type</th>
                     <th scope="col">Time</th>
@@ -46,6 +48,16 @@
                     <tr>
                         <td>{{ $item->service->title }}</td>
                         <td>{{ $item->service_price }}</td>
+                        @if($item->quantity)
+                            <td>{{ $item->quantity }}</td>
+                        @else
+                            <td class="red">No quantity found</td>
+                        @endif
+                        @if($item->total)
+                            <td>{{ $item->total }}</td>
+                        @else
+                            <td class="red">No total found</td>
+                        @endif
                         <td> {{ $item->service_commission }}</td>
                         <td>{{ ucfirst($item->type) }}</td>
                         <td>{{ $item->delivery_time }}</td>
